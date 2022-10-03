71bcfef8e9a22d290a6b55da5ef80350f7cd95f3
Daniel Gomez <dannyelgomezz.13@gmail.com>
2022-10-02 23:22:51 -0500

    Mejorando Registro, Corrigiendo errores

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const { application } = require('express');
+//const bcryptjs = require('bcryptjs');
 const express = require('express');
+const connection = require('../database/db');
 const router = express.Router();
 
 
@@ -9,5 +11,34 @@ router.get('/',(req,res) =>{
 
 router.get('/login',(req,res) =>{
     res.render('login');
+});
+
+router.get('/registro',(req,res) =>{
+    res.render('registro');
+});
+
+//Registro POST
+router.post('/registro', async(req,res,next) => {
+    //capturamos los tres campos
+    const name = req.body.name;
+    const num_ident = req.body.num_ident;
+    const email = req.body.email;
+    //encriptamos la contraseña
+    //let passwordencry = await bcryptjs.hash(pass,8); por si algo jaja y necesitamos
+    connection.query("INSERT INTO pacientes SET ?", {Identificacion:num_ident,nombre:name,email:email}, async(error, results) =>{
+        if(error){
+            console.log(error);
+        }else{
+            res.render('registro',{
+                alert:true,
+                alertTitle: "Registro",
+                alertMessage: "Registro Exitoso",
+                alertIcon:'success',
+                showConfirmButton:false,
+                timer:1500,
+                rute:''
+            });
+        }
+    });
 });
 module.exports = router;
