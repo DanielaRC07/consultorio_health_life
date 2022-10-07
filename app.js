f588e32a1c4f7fa6ce638e4eaea3524432e923ed
Daniel Gomez <dannyelgomezz.13@gmail.com>
2022-10-06 21:49:07 -0500

    Adaptando codigo y estilizando la pagina de inicio

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //importamos express para utlizarlo
 const express = require('express');
+const myconn = require('express-myconnection');
+const mysql = require('mysql');
 const app = express();
 //Hacemos set para la captura de datos del formulario
 app.use(express.urlencoded({extended:false}));
@@ -13,22 +15,29 @@ app.use('/resources', express.static(__dirname + '/public'));
 //utilizamos el motor de platillas
 app.set('view engine', 'ejs');
 //encriptar contraseñas modulo
-const bcryptjs =  require('bcryptjs');
+//const bcryptjs =  require('bcryptjs');
 //varibles de session
-const session = require('express-session');
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+//const session = require('express-session');
+//app.use(session({
+    //secret: 'secret',
+    //resave: true,
+    //saveUninitialized: true
+//}));
 
 //invocamos la conexxion de la DB
 const connection = require('./database/db');
-
+const conexion_2 = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+}
+app.use(myconn(mysql,conexion_2,'single'));
 //rutas
 app.use('/', require('./routes/index'))
 require('./routes/index');
 
-app.listen(3000,(req, res) => {
-    console.log('servidor en ejecucion');
+app.set('port', process.env.PORT ||  3000);
+app.listen(app.get('port'),(req, res) => {
+    console.log('servidor en ejecucion' + app.get('port'));
 })
