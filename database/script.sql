0677d8cb8218201a41d9e08f6ea90a1895e7fe36
daniela rocha <rocha072005@gmail.com>
2022-10-10 12:58:04 -0500

    agrega script database, modicacion de vistas

--- database/script.sql
+++ database/script.sql
@@ -1,8 +1,17 @@
-CREATE TABLE paciente (
-	numero_identificacion varchar(30) NOT NULL,
+CREATE TABLE pacientes (
+	id varchar(30) NOT NULL,
 	nombre varchar(100) NOT NULL,
 	email varchar(100) NOT NULL,
-	CONSTRAINT paciente_PK PRIMARY KEY (numero_identificacion)
+	CONSTRAINT pacientes_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE medicos (
+	id varchar(30) NOT NULL,
+	nombre varchar (100) NOT NULL,
+	CONSTRAINT medicos_PK PRIMARY KEY (id)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -11,9 +20,13 @@ COLLATE=utf8mb4_general_ci;
 CREATE TABLE citas (
 	id INT auto_increment NOT NULL,
 	numero_identificacion varchar(30) NOT NULL,
-	hora_fecha DATETIME NOT NULL,
+	hora TIME (6) NOT NULL,
+	fecha DATE NOT NULL,
+	id_medico varchar(30) NOT NULL,
+	id_paciente varchar(30) NOT NULL,
 	CONSTRAINT citas_PK PRIMARY KEY (id),
-	CONSTRAINT citas_FK FOREIGN KEY (numero_identificacion) REFERENCES paciente(numero_identificacion)
+	CONSTRAINT citas_FK_1 FOREIGN KEY (id_medico) REFERENCES medicos(id),
+	CONSTRAINT citas_FK_2 FOREIGN KEY (id_paciente) REFERENCES pacientes(id)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
